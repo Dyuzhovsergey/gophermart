95899326fd799a6e9c7d1a82afda2a35f0bb5c2a
Sergey <dyuzhovsergey@gmail.com>
2026-01-07 16:32:41 +0300

    added authentication

--- internal/httpserver/deps.go
+++ internal/httpserver/deps.go
@@ -1,7 +1,12 @@
 package httpserver
 
-import "go.uber.org/zap"
+import (
+	"go.uber.org/zap"
+
+	"github.com/Dyuzhovsergey/gophermart/internal/service/auth"
+)
 
 type Deps struct {
 	Logger *zap.Logger
+	Auth   auth.Service
 }
