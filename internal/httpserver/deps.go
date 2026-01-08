264725d927c273beb5c27280ab5fbe7c5fe00e37
Sergey <dyuzhovsergey@gmail.com>
2026-01-08 14:49:13 +0300

    added middleware jwt

--- internal/httpserver/deps.go
+++ internal/httpserver/deps.go
@@ -3,10 +3,12 @@ package httpserver
 import (
 	"go.uber.org/zap"
 
+	"github.com/Dyuzhovsergey/gophermart/internal/middleware"
 	"github.com/Dyuzhovsergey/gophermart/internal/service/auth"
 )
 
 type Deps struct {
 	Logger *zap.Logger
 	Auth   auth.Service
+	JWT    middleware.TokenVerifier
 }
