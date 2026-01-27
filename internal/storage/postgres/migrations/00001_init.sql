5747aae809f75b463440abcf6d95b44da91ee4c9
Sergey <dyuzhovsergey@gmail.com>
2026-01-27 23:00:17 +0300

    added goose migration

--- internal/storage/postgres/migrations/00001_init.sql
+++ internal/storage/postgres/migrations/00001_init.sql
@@ -4,7 +4,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id            BIGSERIAL PRIMARY KEY,
     login         TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
+    password TEXT NOT NULL,
     created_at    TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
